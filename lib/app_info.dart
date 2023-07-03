5cb45b28fcd9d7e36a34760cadf95a43c5c95ea8
akosijho <jhobertpanerio@gmail.com>
2023-07-03 14:46:49 +0800

    complete dependency injection

--- lib/app_info.dart
+++ lib/app_info.dart
@@ -1,27 +1,8 @@
-import 'dart:io';
-
 import 'package:device_info_plus/device_info_plus.dart';
 
 class AppInfo{
 
   DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
 
-  Future<String> deviceInfo() async {
-    if(Platform.isAndroid){
-      AndroidDeviceInfo androidDeviceInfo = await deviceInfoPlugin.androidInfo;
-      return "${androidDeviceInfo.model}  ${androidDeviceInfo.manufacturer}";
-    }
-
-    if(Platform.isLinux){
-      IosDeviceInfo iosDeviceInfo = await deviceInfoPlugin.iosInfo;
-      return "${iosDeviceInfo.model}  ${iosDeviceInfo.systemVersion}";
-    }
-
-    if(Platform.isLinux){
-      LinuxDeviceInfo linuxDeviceInfo = await deviceInfoPlugin.linuxInfo;
-      return "${linuxDeviceInfo.variant}  ${linuxDeviceInfo.prettyName}";
-    }
-
-    return "Device Unsupported";
-  }
+  String get injected => "injected";
 }
--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,8 @@
+import 'package:dependency_injection/app_info.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator(){
-
+  locator.registerFactory(() => AppInfo());
 }
